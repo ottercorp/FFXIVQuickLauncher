29e4d097f50c92ad0b94ad8379c677b9ee56d28a
wozaiha <wozaiha@gmail.com>
2022-05-24 17:59:39 +0800

    首次启动新建桌面快捷方式

--- src/XIVLauncher/Windows/FirstTimeSetupWindow.xaml.cs
+++ src/XIVLauncher/Windows/FirstTimeSetupWindow.xaml.cs
@@ -43,6 +43,27 @@ namespace XIVLauncher.Windows
 #if !XL_NOAUTOUPDATE
             }
 #endif
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);//获取桌面文件夹路径
+            CreateShortcut(desktop, "XIVLauncherCN", Path.Combine(new DirectoryInfo(Environment.CurrentDirectory).Parent.FullName, "XIVLauncherCN.exe"));
+        }
+
+        public static void CreateShortcut(string directory, string shortcutName, string targetPath,
+                                          string description = null, string iconLocation = null)
+        {
+            if (!System.IO.Directory.Exists(directory))
+            {
+                System.IO.Directory.CreateDirectory(directory);
+            }
+
+            string shortcutPath = Path.Combine(directory, string.Format("{0}.lnk", shortcutName));
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath); //创建快捷方式对象
+            shortcut.TargetPath = targetPath; //指定目标路径
+            shortcut.WorkingDirectory = Path.GetDirectoryName(targetPath); //设置起始位置
+            shortcut.WindowStyle = 1; //设置运行方式，默认为常规窗口
+            shortcut.Description = description; //设置备注
+            shortcut.IconLocation = string.IsNullOrWhiteSpace(iconLocation) ? targetPath : iconLocation; //设置图标路径
+            shortcut.Save(); //保存快捷方式
         }
 
         public static string GetShortcutTargetFile(string path)
