cbd4e562acb27a44e47d8493c8747b4c65baf58d
Loskh <1020612624@qq.com>
2022-05-24 15:50:11 +0800

    更新打包

--- release.ps1
+++ release.ps1
@@ -11,4 +11,4 @@ $version = $args[0]
 git add .
 git commit -m "build: ${version}"
 git tag -a -s -m "This is XIVLauncher build ${version}" "${version}"
-git push --atomic origin master ${version}
+git push --atomic origin CN ${version}
