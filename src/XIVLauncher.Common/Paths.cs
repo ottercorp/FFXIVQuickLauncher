a2b05b7de4dcfa8302fc4e446fe2e7ab0c83760e
Tsubasahane <wozaiha@gmail.com>
2024-10-15 09:09:25 +0800

    Roaming目录移动到AppData

--- src/XIVLauncher.Common/Paths.cs
+++ src/XIVLauncher.Common/Paths.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using Serilog;
 
 namespace XIVLauncher.Common
 {
@@ -7,7 +9,46 @@ namespace XIVLauncher.Common
     {
         static Paths()
         {
-            RoamingPath = Path.Combine(new DirectoryInfo(Environment.CurrentDirectory).Parent.FullName, "Roaming");
+            RoamingPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "XIVLauncherCN");
+            CheckPath();
+        }
+
+        public static void CheckPath()
+        {
+            if (!Directory.Exists(Path.Combine(RoamingPath, "addon")))
+            {
+                Log.Warning($"Moving Roaming to AppData");
+                var oldRoamingPath = Path.Combine(new DirectoryInfo(Environment.CurrentDirectory).Parent!.FullName, "Roaming");
+                if (!Directory.Exists(oldRoamingPath)) return;
+
+                Directory.CreateDirectory(RoamingPath);
+                Copy(oldRoamingPath, RoamingPath);
+                //Directory.Delete(oldRoamingPath, true);
+            }
+        }
+
+        private static readonly List<string> Needed = ["addon", "backups", "dalamudAssets", /*"devPlugins",*/ "installedPlugins", "pluginConfigs", "runtime"];
+
+        private static void Copy(string sourcePath, string destPath)
+        {
+            foreach (var file in Directory.GetFiles(sourcePath))
+            {
+                var dest = Path.Combine(destPath, Path.GetFileName(file));
+                File.Copy(file, dest, false);
+            }
+
+            foreach (var directory in Directory.GetDirectories(sourcePath))
+            {
+                if (sourcePath == Path.Combine(new DirectoryInfo(Environment.CurrentDirectory).Parent!.FullName, "Roaming"))
+                {
+                    if (!Needed.Contains(Path.GetFileName(directory)))
+                        continue;
+                }
+
+                var destDir = Path.Combine(destPath, Path.GetFileName(directory));
+                if (!Directory.Exists(destDir)) Directory.CreateDirectory(destDir);
+                Copy(directory, destDir);
+            }
         }
 
         public static string RoamingPath { get; private set; }
